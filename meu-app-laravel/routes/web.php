12227fabe080bb156b15a164a95e722aa3d0c734
arthur-fellipe <arthurfellipe@gmail.com>
2022-06-29 00:42:02 -0300

    Inclui atividades da aula 02 da sprint 09

--- meu-app-laravel/routes/web.php
+++ meu-app-laravel/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    UserController,
+    ViaCepController
+};
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
-// Route::get('/users/{nome}', function ($nome) {
-//     echo $nome;
-// });
+Route::get('/viacep', [ViaCepController::class, 'index'])->name('viacep.index');
+Route::post('/viacep', [ViaCepController::class, 'index'])->name('viacep.index.post');
+Route::get('/viacep/{cep}', [ViaCepController::class, 'show'])->name('viacep.show');

957db82273b7909df34713d010ba9d7214f83761
arthur-fellipe <arthurfellipe@gmail.com>
2022-07-15 18:55:16 -0300

    Inclui atividades da aula 05

--- meu-app-laravel/app/Http/Controllers/UserController.php
+++ meu-app-laravel/app/Http/Controllers/UserController.php
@@ -2,24 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = [
-            'nomes' => [
-                'José Lira',
-                'Jão Lira'
-            ]
-        ];
+        $users = User::all();
 
-        dd($users);
+        return view('users.index', compact('users'));
     }
 
     public function show($id)
     {
-        dd('ID do usuário é '.$id);
+        
+        //$user = User::where('id', $id)->first();        
+        //$user = User::findOrFail($id);
+        
+        if(!$user = User::find($id))
+            return redirect()->route('users.index');
+
+        return view('users.show', compact('user'));
     }
 }
